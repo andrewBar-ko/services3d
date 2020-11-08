71762e0202ed71968142a845e3f0afe8221058a2
Andrew Barkovets <andrewbarkovets@gmail.com>
2020-11-08 12:22:08 +0300

    feat(main.js): new feat (timer)

---
+++ script/main.js
@@ -0,0 +1,49 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    // Таймер 
+    function countTimer(deadline) {
+        let timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        const addZero = (n) => (n < 10 ? "0" + n : n);    
+
+        function getTimeRemaining() {
+
+            let dateStop = new Date(deadline).getTime(),
+                dateNew = new Date().getTime(),
+                timeRemaining = (dateStop - dateNew) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+                // Для отображения дней нужно - hours = Math.floor() % 24;
+                // Потом прописать:
+                // days = Math.floor(timeRemaining / 60 / 60 / 24);
+
+                return { timeRemaining, hours, minutes, seconds };
+
+        }    
+        
+        function updateClock() {
+
+            let timer = getTimeRemaining();
+
+            timerHours.textContent = `${addZero(timer.hours)}`;
+            timerMinutes.textContent = `${addZero(timer.minutes)}`;
+            timerSeconds.textContent = `${addZero(timer.seconds)}`;
+
+            if (timer.timeRemaining > 0) {
+                setInterval(updateClock, 1000);  
+            } else {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+            }
+            
+        }
+        updateClock();
+    }
+
+    setInterval(countTimer, 1000, '10 november 2020');
+});
