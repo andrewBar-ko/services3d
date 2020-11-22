dfd43cfb577c2347f25a21efbbe02bd9128d45dc
andrewBar-ko <andrewbarkovets@gmail.com>
2020-11-22 21:33:26 +0300

    fix(sendForm.js): fix isValidate

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -66,20 +66,29 @@ const sendForm = () => {
     const isValidate = () => {
         document.addEventListener('input', event => {
             const target = event.target;
-            if (target.matches('[name="user_name"]') ||
-            target.matches('[name="user_message"]')) {
+            if (target.matches('[name="user_name"]')) {
+                target.value = target.value.replace(/[^а-яА-ЯёЁ\s]/, '');
+            }
+
+            if (target.matches('[name="user_email"]')) {
+                if (/^[\w-\\.]+@[\w-]+\.[a-z]{2,4}$/i.test(target.value)) {
+                    target.setCustomValidity('');
+                } else {
+                    target.setCustomValidity('Введите значение в формате myemail@mail.ru');
+                }
+            }
+
+            if (target.matches('[name="user_message"]')) {
                 target.value = target.value.replace(/[^а-яА-ЯёЁ,.!?\s]/, '');
-            } else if (target.matches('[name="user_phone"]')) {
+            }
+
+            if (target.matches('[name="user_phone"]')) {
                 target.value = target.value.replace(/[^\\+?[0-9]/i, '');
                 if (/^\+?[78][0-9]{10}$/.test(target.value) ||
                 /^\+?[378][0-9]{11}$/.test(target.value)) {
-                    target.style.cssText = 'border:2px solid green';
                     target.setCustomValidity('');
-                } else if (target.value.length === 0) {
-                    target.style.border = '';
                 } else {
                     target.setCustomValidity('Введите значение в формате +79273335544 или 89273335544');
-                    target.style.cssText = 'border:2px solid red;';
                 }
             }
         });
